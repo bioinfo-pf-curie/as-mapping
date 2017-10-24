77c0954f99d4b455e2b9ab1a73323610da05949d
lsyx <lsyx@u900-bdd-1-202n-6924.curie.fr>
2017-10-24 11:00:37 +0200

    update

--- vcf_filtering/BAMtoVCF.sh
+++ vcf_filtering/BAMtoVCF.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Author(s) : Laur (ne Syx
+# Author(s) : Laurene Syx
 # Contact : laurene.syx@curie.fr
 # Comment(s) : From ChIPseq input to filtered VCF file 
 
@@ -9,7 +9,7 @@ function usage {
     echo -e "Usage : $0"
     echo -e "-c"" <CONFIG file>"
     echo -e "-b"" <ChIPseq input BAM file>"
-    echo -e "-v"" <VCF file>"
+    echo -e "-v"" <SNP list file>"
     echo -e "-o"" <Output directory>"
     echo -e "-h"" <help>"
     exit
@@ -22,7 +22,7 @@ do
     case "$1" in
     (-c) CONFIG=$2; shift;;
     (-b) BAM_FILE=$2; shift;;
-    (-v) VCF_FILE=$2; shift;;
+    (-v) SNP_FILE=$2; shift;;
     (-o) OUT_DIR=$2; shift;;
     (-n) OUT_NAME=$2; shift;;
     (-h) usage;;
@@ -33,22 +33,46 @@ do
     shift
 done
 
-if [[ -z ${CONFIG} ]]; then echo "$0: ERROR - you need to specify a CONFIG file (-c). Exit." 1>&2; exit 1; fi
-source ${CONFIG}
 source /bioinfo/local/curie/ngs-data-management/virtualenvs/hgvs0.3.7/bin/activate
+
+#### Paths #### --------------------------------------------------------------------------
+
+BEDTOOLS_DIR=/bioinfo/local/build/BEDTools/BEDTools_2.25.0/bin
+SAMTOOLS_DIR=/bioinfo/local/build/samtools/samtools-1.1/bin
+PYTHON=/bioinfo/local/build/python/python-2.7.9/bin/python2.7
+SCRIPTS_DIR=/bioinfo/users/lsyx/GitLab/as-mapping/vcf_filtering/scripts
+
+ID_GENO1=   #REF
+ID_GENO2=   #ALT
 
 #### Main #### --------------------------------------------------------------------------
 
-# SAMTools mpileup
+mkdir -p ${OUT_DIR}
 
-${SAMTOOLS_DIR}/samtools mpileup -B -Q 0 -d 100000 -l ${FULL_VCF} -f ${REF_GENO} ${BAM_FILE} > ${OUT_DIR}/${OUT_NAME}.pileup
+# SNP file to BED / reformat SNP file
+zcat ${SNP_FILE} | awk 'BEGIN{OFS="\t"; print "#CHROM","POS","REF","ALT"}{OFS="\t"; split($5,snp,"/");print $2,$3,snp[1],snp[2]}' > ${OUT_DIR}/${OUT_NAME}_SNPlist.txt
+awk -F '\t' '($1!~/^#/){OFS="\t"; print $1,$2}' ${OUT_DIR}/${OUT_NAME}_SNPlist.txt > ${OUT_DIR}/${OUT_NAME}_SNPlist.pos
+
+# BAM sort 
+${SAMTOOLS_DIR}/samtools sort -T ${OUT_DIR}/${OUT_NAME}_sorted_tmp -o ${OUT_DIR}/${OUT_NAME}_sorted.bam ${BAM_FILE}
+
+# SAMTools mpileup
+${SAMTOOLS_DIR}/samtools mpileup -B -Q 0 -d 100000 -l ${OUT_DIR}/${OUT_NAME}_SNPlist.pos -f ${REF_GENO} ${OUT_DIR}/${OUT_NAME}_sorted.bam > ${OUT_DIR}/${OUT_NAME}.pileup
 
 # Count for each base of the VCF file (check_variants.py)
-
 echo -e "${OUT_NAME}\t${OUT_DIR}/${OUT_NAME}.pileup" > ${OUT_DIR}/${OUT_NAME}.conf
 ${PYTHON} ${SCRIPTS_DIR}/clinTools/check_variants.py ${OUT_DIR}/${OUT_NAME}.conf ${REF_GENO} > ${OUT_DIR}/${OUT_NAME}_covInfo.data
 
 # Allele-specific count for each SNP
+awk -F '\t' '(NR>1){OFS="\t";print $2,$3-1,$3,$6,$7,$8,$9}' ${OUT_DIR}/${OUT_NAME}_covInfo.data > ${OUT_DIR}/${OUT_NAME}_covInfo_tmp.data
+mv ${OUT_DIR}/${OUT_NAME}_covInfo_tmp.data ${OUT_DIR}/${OUT_NAME}_covInfo.data
+${PYTHON} ${SCRIPTS_DIR}/alleleCount.py -f ${OUT_DIR}/${OUT_NAME}_covInfo.data -v ${OUT_DIR}/${OUT_NAME}_SNPlist.txt -1 ${ID_GENO1} -2 ${ID_GENO2} -o ${OUT_DIR}/ -n ${OUT_NAME}_SNPInfo
 
-awk '(NR>1){OFS="\t";print $2,$3-1,$3,$4,$5,$6,$7,$8,$9,$2,$3-1,$3,"NoName",0,"+"}' ${OUT_DIR}/${OUT_NAME}_covInfo.data > ${OUT_DIR}/${OUT_NAME}_covGene.data
-${PYTHON} ${SCRIPTS_DIR}/alleleCount.py --r1 ${OUT_DIR}/${OUT_NAME}_covGene.data --strand 0 -v ${SNP_FILE} -1 ${ID_GENO1} -2 ${ID_GENO2} -o ${OUT_DIR} -n ${OUT_NAME}
+# Heterozygous SNPs
+awk -F '\t' '(NR>1){OFS="\t"; totalCount=$4+$9; if(totalCount>=10){print $1, $2, $3, totalCount, $6/totalCount, $8/totalCount, $9/totalCount}}' ${OUT_DIR}/${OUT_NAME}_SNPInfo.data  | awk -F '\t' '($1!="chrY" && $1!="chrM" && $7<=0.1 && $5>=0.25 && $5<=0.75 && $6>=0.25 && $6<=0.75){OFS="\t"; print $1,$2,$3}' > ${OUT_DIR}/${OUT_NAME}.bed
+
+# Generate the clean SNP file
+ awk -F '\t' '(NR>1){OFS="\t"; print $1, $2-1, $2, $3, $4}' ${OUT_DIR}/${OUT_NAME}_SNPlist.txt | ${BEDTOOLS_DIR}/intersectBed -a stdin -b ${OUT_DIR}/${OUT_NAME}.bed -wa | awk -F '\t' '{OFS="\t"; sub("chr","",$1); print 1,$1,$3,1,$4"/"$5}' > ${OUT_DIR}/${OUT_NAME}.txt
+
+# Remove all tmp files
+rm ${OUT_DIR}/${OUT_NAME}_SNPlist.txt ${OUT_DIR}/${OUT_NAME}_SNPlist.pos ${OUT_DIR}/${OUT_NAME}_sorted.bam ${OUT_DIR}/${OUT_NAME}.pileup ${OUT_DIR}/${OUT_NAME}_covInfo.data ${OUT_DIR}/${OUT_NAME}.conf ${OUT_DIR}/${OUT_NAME}_SNPInfo.data ${OUT_DIR}/${OUT_NAME}.bed 
--- vcf_filtering/scripts/alleleCount.py
+++ vcf_filtering/scripts/alleleCount.py
@@ -8,23 +8,23 @@
 ## Author(s): Kenzo-Hugo Hillion & Laurène Syx
 ## Contact(s): kenzo.hillion@curie.fr & laurene.syx@curie.fr
 ## Python version: 2.7.9
-## Script description: Create an allele-specific count table 
+## Script description: Create an allele-specific count table per SNP
 
-scriptVersion = '0.1 - 06-17-2016'
+scriptVersion = '10-23-2017'
 
 """
 This script is used to create an allele-specific count table 
 
     INPUTS :  
-        - Output file(s) (2 files in case of paired-end sequencing: R1 and R2) from check_variants.py (clinTools) intersected with a BED6 file 
-        - VCF file
+        - Output file from check_variants.py (clinTools) intersected with a BED6 file 
+        - SNP file
         - Parent1 and Parent2 names
         
     OUTPUT :
         - Allele-specific count table
     
     USE :
-        ./alleleCount.py [opts: --r2 clinToolsFile2 --strand 0:unstranded(default)/1:R1Reverse/2:R1Forward] --r1 clinToolsFile1 -v vcfFile -1 parent1 -2 parent2
+        ./alleleCount.py [opts: --r1 clinToolsFile1 -v vcfFile -1 parent1 -2 parent2]
 """
 
 ###########  Import  ###########
@@ -35,7 +35,6 @@ import subprocess
 import time
 import sys
 import re
-import pandas as pd
 
 ###########  Function  ###########
 
@@ -54,106 +53,21 @@ def testNone(argument):
         variable = ""
     return variable
 
-def alleleCount(r1, strand):
+def alleleCount(r1):
     with open(r1, "r") as clinFile:
         snpCount = dict()
         for clinLine in clinFile: 
-            if clinLine.startswith('#'):
-                pass  
-            else:
-                baseInfo = clinLine.rstrip().split("\t")
-                curChrm = baseInfo[0]
-                curStart = baseInfo[1]
-                curEnd = baseInfo[2]              
-                if strand == 1: 
-                    curGene = baseInfo[16]
-                    curStrand = baseInfo[18]
-                    snpID = str(curChrm) + "_" + str(curStart)+ "." + str(curGene) 
-                    if curStrand == "+":
-                        countA = baseInfo[6]
-                        countT = baseInfo[8]
-                        countC = baseInfo[10]
-                        countG = baseInfo[12]
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}
-                    else:
-                        countA = baseInfo[5]
-                        countT = baseInfo[7]
-                        countC = baseInfo[9]
-                        countG = baseInfo[11]
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}     
-                elif strand == 2:
-                    curGene = baseInfo[16]
-                    curStrand = baseInfo[18]
-                    snpID = str(curChrm) + "_" + str(curStart)+ "." + str(curGene) 
-                    if curStrand == "+":
-                        countA = baseInfo[5]
-                        countT = baseInfo[7]
-                        countC = baseInfo[9]
-                        countG = baseInfo[11]
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}
-                    else:
-                        countA = baseInfo[6]
-                        countT = baseInfo[8]
-                        countC = baseInfo[10]
-                        countG = baseInfo[12]
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}     
-                else:
-                    curGene = baseInfo[12]
-                    curStrand = baseInfo[14]
-                    snpID = str(curChrm) + "_" + str(curStart)+ "." + str(curGene) 
-                    countA = baseInfo[5]
-                    countT = baseInfo[6]
-                    countC = baseInfo[7]
-                    countG = baseInfo[8]
-                    snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}
+            baseInfo = clinLine.rstrip().split("\t")
+            curChrm = baseInfo[0]
+            curStart = baseInfo[1]
+            curEnd = baseInfo[2]              
+            snpID = str(curChrm) + "_" + str(curStart)
+            countA = baseInfo[3]
+            countT = baseInfo[4]
+            countC = baseInfo[5]
+            countG = baseInfo[6]
+            snpCount[snpID] = {"Chr":curChrm,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "C":countC, "G":countG}
         return snpCount                
-
-def alleleCountStrandPaired(r1, r2, strand, outname, output_dir):
-    file1 = pd.read_csv(r1,sep="\t")
-    file2 = pd.read_csv(r2,sep="\t")
-    mergeFile = pd.merge(file1, file2, on=["#chromosome", "start", "end", "gene_name", "score", "strand"], how="outer")
-    mergeFile = mergeFile.fillna(0)
-    mergeFile.to_csv(str(output_dir) + str(outname) + "_mergeCovGene.data",sep="\t",index=False)
-    snpCount = dict()
-    with open(str(output_dir) + str(outname) + "_mergeCovGene.data", "r") as mergeCovFile:
-        for mergeLine in mergeCovFile: 
-            if mergeLine.startswith('#'):
-                pass  
-            else:            
-                baseInfo = mergeLine.rstrip().split("\t")
-                curChrm = baseInfo[0]
-                curStart = int(float(baseInfo[1]))
-                curEnd = int(float(baseInfo[2]))
-                curGene = baseInfo[16]
-                curStrand = baseInfo[18]
-                snpID = str(curChrm) + "_" + str(curStart)+ "." + str(curGene)
-                if strand == 1:
-                    if curStrand == "+":
-                        countA = int(float(baseInfo[6])) + int(float(baseInfo[21]))
-                        countT = int(float(baseInfo[8])) + int(float(baseInfo[23]))
-                        countC = int(float(baseInfo[10])) + int(float(baseInfo[25]))
-                        countG = int(float(baseInfo[12])) + int(float(baseInfo[27]))
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}
-                    else:
-                        countA = int(float(baseInfo[5])) + int(float(baseInfo[22]))
-                        countT = int(float(baseInfo[7])) + int(float(baseInfo[24]))
-                        countC = int(float(baseInfo[9])) + int(float(baseInfo[26]))
-                        countG = int(float(baseInfo[11])) + int(float(baseInfo[28]))
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}     
-                elif strand == 2:
-                    if curStrand == "+":
-                        countA = int(float(baseInfo[5])) + int(float(baseInfo[22]))
-                        countT = int(float(baseInfo[7])) + int(float(baseInfo[24]))
-                        countC = int(float(baseInfo[9])) + int(float(baseInfo[26]))
-                        countG = int(float(baseInfo[11])) + int(float(baseInfo[28]))
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}  
-                    else:
-                        countA = int(float(baseInfo[6])) + int(float(baseInfo[21]))
-                        countT = int(float(baseInfo[8])) + int(float(baseInfo[23]))
-                        countC = int(float(baseInfo[10])) + int(float(baseInfo[25]))
-                        countG = int(float(baseInfo[12])) + int(float(baseInfo[27]))
-                        snpCount[snpID] = {"Chr":curChrm,"Gene":curGene,"Strand":curStrand,"Start":curStart ,"End":curEnd ,"A":countA, "T":countT, "G":countG, "C":countC}   
-        return snpCount
             
 def getGenotypes(vcf):
     with open(vcf, "r") as vcfFile:
@@ -166,10 +80,10 @@ def getGenotypes(vcf):
                 snpChr = str(snpInfo[0])
                 snpStart = int(snpInfo[1])-1
                 snpEnd = snpInfo[1]
-                snpParent1 = snpInfo[3]
-                snpParent2 = snpInfo[4]
+                snpGeno1 = snpInfo[2]
+                snpGeno2 = snpInfo[3]
                 snpID = str(snpChr) + "_" + str(snpStart)
-                vcfDict[snpID] = {"parent1":snpParent1, "parent2":snpParent2}
+                vcfDict[snpID] = {"geno1":snpGeno1, "geno2":snpGeno2}
         return vcfDict
 
 ## Main
@@ -180,39 +94,26 @@ if __name__ == "__main__":
                                      epilog = 'Version '+ scriptVersion)
 
     parser.add_argument("-f", "--r1", type=str, help="R1_file generated by check_variants.py") 
-    parser.add_argument("-r", "--r2", type=str, help="R2_file generated by check_variants.py") 
     parser.add_argument("-v", "--vcf", type=str, action="store", help="VCF file")
-    parser.add_argument("-1", "--parent1", type=str, action="store",help="Parent 1")
-    parser.add_argument("-2", "--parent2", type=str, action="store",help="Parent 2")
-    parser.add_argument("-s", "--strand", type = int, help="0 for unstranded seq (default) \
-                        / 1 for R1 from a reverse strand / 2 for R1 from a forward strand")
-    parser.add_argument('-n', '--name', type = str, \
-                        help = 'prefix name for the output file (default : SNPCountTable')
-    parser.add_argument('-o', '--output_dir', type = str, \
-                        help = 'output directory (default : current directory)')
+    parser.add_argument("-1", "--geno1", type=str, action="store",help="Geno 1")
+    parser.add_argument("-2", "--geno2", type=str, action="store",help="Geno 2")
+    parser.add_argument("-n", "--name", type = str, \
+                        help = "prefix name for the output file (default : SNPCountTable")
+    parser.add_argument("-o", "--output_dir", type = str, \
+                        help = "output directory (default : current directory)")
 
     args = parser.parse_args()
 
     ## Store arguments
     r1 = args.r1
     vcf = args.vcf
-    parent1 = args.parent1
-    parent2 = args.parent2
-    
-    if (args.r2):
-        r2 = args.r2
-    else:
-        r2 = None
+    geno1 = args.geno1
+    geno2 = args.geno2
 
     if (args.name):
         outname = args.name
     else:
         outname = "SNPCountTable"
-
-    if ((args.strand == 0) |(args.strand == 1) | (args.strand == 2)):
-        strand = args.strand
-    else:
-        strand = 0
 
     if (args.output_dir):
         output_dir = args.output_dir
@@ -223,25 +124,19 @@ if __name__ == "__main__":
 
     ## Allele-specific count (per SNP)
     countTable = open(str(output_dir) + str(outname) + ".data", "w")
-    countTable.write("\t".join(map(str,["chromosome", "start", "end", "gene","strand","total", str(parent1)+"_base", str(parent1)+"_count", str(parent2)+"_base", str(parent2)+"_count", "otherCount", "otherBase"]))+"\n")
+    countTable.write("\t".join(map(str,["chromosome", "start", "end", "gene","strand","total", str(geno1)+"_base", str(geno1)+"_count", str(geno2)+"_base", str(geno2)+"_count", "otherCount", "otherBase"]))+"\n")
 
-    if r2 is None:    #If unstranded sequencing or if only stranded single-end sequencing 
-        snpCount = alleleCount(r1, strand)
-        vcfDict = getGenotypes(vcf)
-    else: #If stranded paired-end sequencing
-        snpCount = alleleCountStrandPaired(r1, r2, strand,outname, output_dir)
-        vcfDict = getGenotypes(vcf)
+    snpCount = alleleCount(r1)
+    vcfDict = getGenotypes(vcf)
 
-    for ID, value in zip(snpCount.keys(), snpCount.values()):
-        snpID = ID.split(".")[0]
+    for snpID, value in zip(snpCount.keys(), snpCount.values()):
         if snpID in vcfDict:
-            snpParent1 = vcfDict[snpID]["parent1"]
-            snpParent2 = vcfDict[snpID]["parent2"]
+            snpGeno1 = vcfDict[snpID]["geno1"]
+            snpGeno2 = vcfDict[snpID]["geno2"]
             total = int(value["A"])+int(value["T"])+int(value["C"])+int(value["G"])
-            parentCoverage = int(value[snpParent1])+int(value[snpParent2])
+            parentCoverage = int(value[snpGeno1])+int(value[snpGeno2])
             other = int(total)-int(parentCoverage)
-            otherBase =  list(set(["A","T","C","G"])-set([snpParent1,snpParent2]))  
+            otherBase =  list(set(["A","T","C","G"])-set([snpGeno1,snpGeno2]))  
             otherCount = ",".join(map(str,[otherBase[0]+":"+str(value[otherBase[0]]), otherBase[1]+":"+str(value[otherBase[1]])]))
-            countTable.write("\t".join([str(value["Chr"]), str(value["Start"]),str(value["End"]),str(value["Gene"]), str(value["Strand"]), \
-                         str(parentCoverage), str(snpParent1), str(value[snpParent1]), str(snpParent2), str(value[snpParent2]), str(other), str(otherCount)])+"\n")
+            countTable.write("\t".join([str(value["Chr"]), str(value["Start"]),str(value["End"]), str(parentCoverage), str(snpGeno1), str(value[snpGeno1]), str(snpGeno2), str(value[snpGeno2]), str(other), str(otherCount)])+"\n")
     countTable.close()
